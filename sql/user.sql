0f8c2b2a8e924cc85bc002b5bc571336b023765f
Sora22 <Sahra@192.41.132.105>
2015-11-17 11:41:32 +0100

    added database information

---
+++ sql/user.sql
@@ -0,0 +1,56 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 17. Nov 2015 um 11:38
+-- Server Version: 5.6.21
+-- PHP-Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `fhweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `Name` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Passwort` varchar(255) DEFAULT NULL,
+  `Type` enum('admin','fh') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`Name`, `Email`, `Passwort`, `Type`) VALUES
+('FHNW', 'sahra.kaesermann@students.fhnw.ch', '23456789', 'fh'),
+('Admin', 'sahra@kaesermann.ch', '12345678', 'admin');
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`Email`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
