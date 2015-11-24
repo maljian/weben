6e292f7dbc1662f901dea2412458737914b6203c
Sora22 <Sahra@Study.edu.ds.fhnw.ch>
2015-11-24 14:05:32 +0100

    update layout

--- course.php
+++ course.php
@@ -1,24 +1,26 @@
-<?php
-    session_start();
-    include ("Layout/header.html");?>
+    <?php
+        session_start();
+        include ("Layout/header.html");
+        include "db.inc.php";
+        if (isset($_SESSION['eingeloggt'])){
+         if($_SESSION['eingeloggt']==true){
+              include ("Layout/nav-loggedin.html");
+            }
+         else{
+             include ("Layout/nav.html");  
+            }
+        }
+        else{
+            include ("Layout/nav.html");
+        }
+    ?>
         <!-- Main content -->
         <div class = "col-md-7" id="mainBody">
             <h1>Kurs</h1>
         </div>
     <?php
-    
-    include "db.inc.php";
-    if (isset($_SESSION['eingeloggt'])){
-     if($_SESSION['eingeloggt']==true){
-          include ("Layout/loginbereich.html"); 
-        }
-     else{
-         include ("Layout/login.html");  
-        }
-    }
-    else{
-        include ("Layout/login.html");
-    }
-    include ("Layout/ads.html");
-    include ("Layout/footer.html");?>
+        include ("Layout/sidebar.html");
+        include ("Layout/ads.html");
+        include ("Layout/footer.html");
+    ?>
 </html>
--- fhprofil.php
+++ fhprofil.php
@@ -1,31 +1,26 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <?php 
+    <?php
         session_start();
         include ("Layout/header.html");
-        ?>
-        <!-- Main content -->
-        <div class = "col-md-7" id="mainBody">
-            <h1>FHNW</h1>
-        </div>
-     <?php 
         include "db.inc.php";
         if (isset($_SESSION['eingeloggt'])){
          if($_SESSION['eingeloggt']==true){
-              include ("Layout/loginbereich.html"); 
+              include ("Layout/nav-loggedin.html");
             }
          else{
-             include ("Layout/login.html");  
+             include ("Layout/nav.html");  
             }
         }
         else{
-            include ("Layout/login.html");
-        }include ("Layout/ads.html");
+            include ("Layout/nav.html");
+        }
+    ?>
+    <!-- Main content -->
+    <div class = "col-md-7" id="mainBody">
+        <h1>FHNW</h1>
+    </div>
+    <?php
+        include ("Layout/sidebar.html");
+        include ("Layout/ads.html");
         include ("Layout/footer.html");
     ?>
 </html>
