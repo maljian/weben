5102c6813db5b42341f27ee2a8ac929af5cf9402
Sora22 <Sahra@Study.edu.ds.fhnw.ch>
2015-11-24 14:29:14 +0100

    filter update

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
     ?>
     <!-- Main content -->
     <div class = "col-md-7" id="mainBody">
-        <h1>Suchen und Finden Sie jetzt den passenden Bachelor- , Master- oder Weiterbildungskurs</h1>
+        <h2>Suchen und Finden Sie jetzt den passenden Bachelor- , Master- oder Weiterbildungskurs</h2>
         <form role="form">
             <div class="form-group">
                 <div class="col-md-4">
-                    <label for="ort">Ort:</label>
+                    <label for="ort">Region:</label>
                     <select class="form-control text-center" id="ort">
                         <option> --------- Auswahl --------- </option>
                         <option>Nordwestschweiz</option>
@@ -28,6 +28,36 @@
                         <option>Raum Z&uuml;rich</option>
                         <option>Raum Bern</option>
                     </select>
+                    <label for="fh">Fachhochschule:</label>
+                    <select class="form-control text-center" id="fh">
+                        <option> --------- Auswahl --------- </option>
+                        <option>FHNW</option>
+                        <option>ETH</option>
+                        <option>HSLU</option>
+                        <option>BFH</option>
+                    </select>
+                    <?php
+                        /**<label for="bachelor">Bachelorstudiengang:</label>
+                        <select class="form-control text-center" id="bachelor">
+                            <option> --------- Auswahl --------- </option>
+                            <option>Wirtschaftsinformatik</option>
+                            <option>Betriebs&ouml;konomie</option>
+                            <option>International Management</option>
+                            <option>International Business Management</option>
+                        </select>
+                        <label for="weiterb">Weiterbildung:</label>
+                        <select class="form-control text-center" id="weiterb">
+                            <option> --------- Auswahl --------- </option>
+                            <option>Wirtschaftsinformatik und E-Business</option>
+                            <option>Human Resource Management</option>
+                        </select>
+                        <label for="master">Masterstudiengang:</label>
+                        <select class="form-control text-center" id="master">
+                            <option> --------- Auswahl --------- </option>
+                            <option>International Management</option>
+                            <option>Business Information Systems</option>
+                        </select>*/
+                    ?>
                 </div>
                 <div class="col-md-4">
                     <label for="fachbereich">Fachbereich:</label>
@@ -43,22 +73,6 @@
                         <option>P&auml;dagogik</option>
                         <option>Soziale Arbeit</option>
                     </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-4">
-                    <label for="fh">Fachhochschule:</label>
-                    <select class="form-control text-center" id="fh">
-                        <option> --------- Auswahl --------- </option>
-                        <option>FHNW</option>
-                        <option>ETH</option>
-                        <option>HSLU</option>
-                        <option>BFH</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-4">
                     <label for="studiengang">Studiengang:</label>
                     <select class="form-control text-center" id="studiengang">
                         <option> --------- Auswahl --------- </option>
@@ -67,8 +81,6 @@
                         <option>Weiterbildung</option>
                     </select>
                 </div>
-            </div>
-            <div class="form-group">
                 <div class="col-md-4">
                     <label for="stform">Studienform:</label>
                     <select class="form-control text-center" id="stform">
@@ -77,38 +89,6 @@
                         <option>Pr&auml;senzstudium Teilzeit</option>
                         <option>Fernstudium Vollzeit</option>
                         <option>Fernstudium Teilzeit</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-4">
-                    <label for="bachelor">Bachelorstudiengang:</label>
-                    <select class="form-control text-center" id="bachelor">
-                        <option> --------- Auswahl --------- </option>
-                        <option>Wirtschaftsinformatik</option>
-                        <option>Betriebs&ouml;konomie</option>
-                        <option>International Management</option>
-                        <option>International Business Management</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-4">
-                    <label for="master">Masterstudiengang:</label>
-                    <select class="form-control text-center" id="master">
-                        <option> --------- Auswahl --------- </option>
-                        <option>International Management</option>
-                        <option>Business Information Systems</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-4">
-                    <label for="weiterb">Weiterbildung:</label>
-                    <select class="form-control text-center" id="weiterb">
-                        <option> --------- Auswahl --------- </option>
-                        <option>Wirtschaftsinformatik und E-Business</option>
-                        <option>Human Resource Management</option>
                     </select>
                 </div>
             </div>
