85984f779907bf44489df6dcaf8ebb2cf7d29e76
Nadine <nadine.toepfer@students.fhnw.ch>
2015-12-15 20:28:39 +0100

    Werbung anmelden überabeitet

--- buyAd.php
+++ buyAd.php
@@ -1,53 +1,65 @@
 <?php
-if(!empty($_POST)){
-$gender = $_POST['gender'];
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$street = $_POST['street'];
-$plz = $_POST['plz'];
-$city = $_POST['city'];
-$phonenumber = $_POST['phonenumber'];
-$email = $_POST['email'];
-$image = $_POST['image'];
-$myAttachement = chunk_split(base64_encode(file_get_contents($image)));
+if (!empty($_POST)) {
+    $gender = $_POST['gender'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $street = $_POST['street'];
+    $plz = $_POST['plz'];
+    $city = $_POST['city'];
+    $phonenumber = $_POST['phonenumber'];
+    $email = $_POST['email'];
+    $image = chunk_split(base64_encode(file_get_contents($image)));
+
+    include "db.inc.php";
+    $link = mysqli_connect("localhost", $benutzer, $passwort) or die("Keine Verbindung zur Datenbank!");
+    mysqli_select_db($link, $dbname) or die("Datenbank nicht gefunden!" . mysql_error());
+
+    // damit ä,ö,ü und é richtig dargestellt werden! --> auf utf8 stellen
+    mysqli_set_charset($link, 'utf8');
+
+    $abfrage = "INSERT INTO `ads`(`id`, `gender`, `firstname`, `lastname`, `street`, `plz`, `city`, `email`, `phonenumber`, `image`) VALUES 
+                ('','$gender','$firstname','$lastname','$street','$plz','$city','$email','$phonenumber','$image')";
+    $ergebnis = mysqli_query($link, $abfrage);
+    if (!$ergebnis) {
+        die('Could not connect: ' . mysql_error());
+    }
+    mysqli_close($link);
 
 //Quelle: http://wiki.selfhtml.org/wiki/PHP/Anwendung_und_Praxis/Formmailer-Advanced
 // eigene Mailadresse
-$zieladresse = 'fhnw.weben@gmail.com';
+    $zieladresse = 'fhnw.weben@gmail.com';
 
 //Absenderadresse
-$absenderadresse = $_POST['email'];
+    $absenderadresse = $_POST['email'];
 
 //Absendername
-$absendername = $firstname." ".$lastname;
+    $absendername = $firstname . " " . $lastname;
 
 //Betreff Empfänger und Absender
-$betreff = 'Werbefläche FH Portal mieten';
+    $betreff = 'Werbefläche FH Portal mieten';
 
 //Weiterleitung nach Absenden
-$urlDankeSeite = '';
+    $urlDankeSeite = '';
 
 // Welches Zeichen soll zwischen dem Feldnamen und dem angegebenen Wert stehen
-$trenner = ":\t"; // Doppelpunkt und Tabulator
+    $trenner = ":\t"; // Doppelpunkt und Tabulator
 
-/**
- * Ende Konfigurator
- */
+    /**
+     * Ende Konfigurator
+     */
+    require_once "swiftmailer/lib/swift_required.php"; // Swift initialisieren
 
-require_once "swiftmailer/lib/swift_required.php"; // Swift initialisieren
+    if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
-if ($_SERVER['REQUEST_METHOD']==="POST"){
-    
-    $message = Swift_Message::newInstance(); // Ein Objekt für die Mailnachricht
-    
-    $message
-            ->setFrom(array($absenderadresse => $absendername))
-            ->setTo(array($zieladresse))
-            ->setBcc(array($absenderadresse))
-            ->setSubject($betreff)
-            ->setBody(
-    
-"Anfrage FH Portal Werbung schalten:
+        $message = Swift_Message::newInstance(); // Ein Objekt für die Mailnachricht
+
+        $message
+                ->setFrom(array($absenderadresse => $absendername))
+                ->setTo(array($zieladresse))
+                ->setBcc(array($absenderadresse))
+                ->setSubject($betreff)
+                ->setBody(
+                        "Anfrage FH Portal Werbung schalten:
 Vorname : $firstname
 Nachname: $lastname
 Strasse: $street
@@ -58,63 +70,61 @@ Telefonnummer: $phonenumber
     
 Bild: 
 $image");
-    
-    $mailtext = "";
-    
-    foreach ($_POST as $name => $wert){
-        if(is_array($wert)){
-            foreach ($wert as $einzelwert){
-                $mailtext .= $name.$trenner.$einzelwert."\n";
+
+        $mailtext = "";
+
+        foreach ($_POST as $name => $wert) {
+            if (is_array($wert)) {
+                foreach ($wert as $einzelwert) {
+                    $mailtext .= $name . $trenner . $einzelwert . "\n";
+                }
+            } else {
+                $mailtext .= $name . $trenner . $wert . "\n";
             }
-        } else {
-            $mailtext .= $name.$trenner.$wert."\n";
         }
+
+        // Code Ergänzungen: try und catch, logger
+        // gemäss: [http://swiftmailer.org/pdf/Swiftmailer.pdf Swiftmailer.pdf]
+
+        try {
+            /* Mit try und catch können die Fehlerevents differenzierter getestet werden
+             *
+             * diverse Möglichkeiten für $Transport 
+             * smtp mit den gewohnten Optionen, siehe Dokumentation Swiftmailer  
+             *  für TLS und SSL  allenfalls phpinfo ob extentiens installiert
+             */
+
+            $Transport0 = Swift_MailTransport::newInstance();        /* Beispiel geht über PHP-Mail, geht i.a. 
+              aber keine Information von logger */
+
+            $Transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, 'tls') /* 'tls', Ports je nach Server */
+                    ->setUsername("fhnw.weben@gmail.com")
+                    ->setPassword("!Je8Na8Sa9!");
+
+            $Transport2 = Swift_SmtpTransport::newInstance('mail.gmail.com', 995, 'tls') /* 'tls' */
+                    ->setUsername("...")
+                    ->setPassword("...");
+
+            $mailer = Swift_Mailer::newInstance($Transport);
+
+            // Echo Logger aktivieren (es gibt noch einen logger der auf File schreibt)
+            $logger = new Swift_Plugins_Loggers_EchoLogger();
+            $mailer->registerPlugin(new Swift_Plugins_LoggerPlugin($logger));
+
+            $result = $mailer->send($message);
+        } catch (Exception $e) {
+            $error_log = $logger->dump();
+        }
+
+        if ($result == 0) {
+            die("Mail konnte nicht versandt werden.");
+        }
+        header("Location: $urlDankeSeite");
+        exit;
     }
-    
-    // Code Ergänzungen: try und catch, logger
- // gemäss: [http://swiftmailer.org/pdf/Swiftmailer.pdf Swiftmailer.pdf]
- 
- try {
-     /* Mit try und catch können die Fehlerevents differenzierter getestet werden
-     *
-     * diverse Möglichkeiten für $Transport 
-     * smtp mit den gewohnten Optionen, siehe Dokumentation Swiftmailer  
-     *  für TLS und SSL  allenfalls phpinfo ob extentiens installiert
-     */
- 
-     $Transport0 = Swift_MailTransport::newInstance();        /* Beispiel geht über PHP-Mail, geht i.a. 
-                                                                 aber keine Information von logger     */
- 
-     $Transport = Swift_SmtpTransport::newInstance('smtp.gmail.com',587,'tls' )     /* 'tls', Ports je nach Server */
-      ->setUsername("fhnw.weben@gmail.com")
-      ->setPassword("!Je8Na8Sa9!");
-     
-     $Transport2 = Swift_SmtpTransport::newInstance('mail.gmail.com',995,'tls' )  /* 'tls' */
-      ->setUsername("...")
-      ->setPassword("...");
- 
-     $mailer = Swift_Mailer::newInstance($Transport); 
- 
-     // Echo Logger aktivieren (es gibt noch einen logger der auf File schreibt)
-     $logger = new Swift_Plugins_Loggers_EchoLogger();
-     $mailer -> registerPlugin ( new Swift_Plugins_LoggerPlugin ($logger));
- 
-     $result = $mailer->send($message);
- 
-    }
-     catch(Exception $e){
-       $error_log = $logger->dump();
-    }
-    
-    if ($result == 0){
-        die("Mail konnte nicht versandt werden.");
-    }
-    header("Location: $urlDankeSeite");
-    exit;
-}
 //echo $message->toString();
 
-header("Content-type: text/html; charset=utf-8");
+    header("Content-type: text/html; charset=utf-8");
 }
 session_start();
 include("login/header.php");
@@ -181,7 +191,7 @@ include("login/header.php");
                 <input type="tel" class="form-control" id="phonenumber" name="phonenumber">
             </div>
         </div>
-         <div class="form-group">
+        <div class="form-group">
             <label class="control-label col-sm-2" for="image">Bilddatei: *</label>
             <div class="col-sm-4">
                 <input type="file" class="form-control" id="image" name="image" required>
