3505d075c42e2b162a263cb786341a1ceb5339e8
Nadine <nadine.toepfer@students.fhnw.ch>
2015-12-18 12:11:15 +0100

    Einzahlungsschein übernimmt Variablen

--- pdf/einzahlungsschein.php
+++ pdf/einzahlungsschein.php
@@ -1,7 +1,26 @@
 <?php
+session_start();
 require_once('class.einzahlungsschein.php');
 require_once('../fpdf/fpdf.php');
 
+require '../database.php';
+    $id = null;
+    if (!empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+
+ if (null == $id) {
+        header("Location: ../addAd.php");
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM ads where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        Database::disconnect();
+    }
+    
 $amount= "150.20";
 $ref="5000001195";
 
@@ -17,7 +36,7 @@ $pdf->Cell(50, 4, "Just some dummy text.");
 $ezs = new Einzahlungsschein(196, 0, $pdf);
 $ezs->setBankData("Berner Kantonalbank AG", "3001 Bern", "01-200000-7");
 $ezs->setRecipientData("FH Portal", "Von-Roll-Strasse 10", "4600 Olten", "123456");
-$ezs->setPayerData("Heinz Müller", "Beispielweg 23", "3072 Musterlingen");
+$ezs->setPayerData( $data['firstname']." ".$data['lastname'], $data['street'], $data['plz']." ".$data['city']);
 $ezs->setPaymentData($amount, $ref);
 $ezs->createEinzahlungsschein(false, true);
 
