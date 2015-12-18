d226ffc6749fb0505ca03fdbbd4b97df6cc84722
Jenny <Jenny@jennys-mbp.edu.ds.fhnw.ch>
2015-12-18 16:48:09 +0100

    reset email sending changed

--- login/login_reset.php
+++ login/login_reset.php
@@ -32,6 +32,98 @@ if (isset($_POST['resetEmail']) AND ($_POST['resetEmail']!=''))
         $header = "From: fhnw.weben@gmail.com";
         @mail($email,$betreff,$inhalt,$header);
   
+        if(!empty($_POST)){
+                //Quelle: http://wiki.selfhtml.org/wiki/PHP/Anwendung_und_Praxis/Formmailer-Advanced
+                // eigene Mailadresse
+                $zieladresse = $email;
+
+                //Absenderadresse
+                $absenderadresse = 'fhnw.weben@gmail.com';
+
+                //Absendername
+                $absendername = "FH Portal";
+
+                //Betreff Empfänger und Absender
+                $betreff = 'Neues Passwort vom FH Portal';
+
+                // Welches Zeichen soll zwischen dem Feldnamen und dem angegebenen Wert stehen
+                $trenner = ":\t"; // Doppelpunkt und Tabulator
+
+                /**
+                 * Ende Konfigurator
+                 */
+
+                require_once "../swiftmailer/lib/swift_required.php"; // Swift initialisieren
+
+                if ($_SERVER['REQUEST_METHOD']==="POST"){
+
+                    $message = Swift_Message::newInstance(); // Ein Objekt für die Mailnachricht
+
+                    $message
+                            ->setFrom(array($absenderadresse => $absendername))
+                            ->setTo(array($zieladresse))
+                            ->setSubject($betreff)
+                            ->setBody("Sehr geehrte Kundin\nSehr geehrter Kunde\n\nHier Ihr neues Passwort: '$passwort'\n
+                                      Freundliche Grüsse\nIhr FH-Portal-Team\nwww.dine.bronxx.org");
+
+                    $mailtext = "";
+
+                    foreach ($_POST as $name => $wert){
+                        if(is_array($wert)){
+                            foreach ($wert as $einzelwert){
+                                $mailtext .= $name.$trenner.$einzelwert."\n";
+                            }
+                        } else {
+                            $mailtext .= $name.$trenner.$wert."\n";
+                        }
+                    }
+
+                    // Code Ergänzungen: try und catch, logger
+                 // gemäss: [http://swiftmailer.org/pdf/Swiftmailer.pdf Swiftmailer.pdf]
+
+                 try {
+                     /* Mit try und catch können die Fehlerevents differenzierter getestet werden
+                     *
+                     * diverse Möglichkeiten für $Transport 
+                     * smtp mit den gewohnten Optionen, siehe Dokumentation Swiftmailer  
+                     *  für TLS und SSL  allenfalls phpinfo ob extentiens installiert
+                     */
+
+                     $Transport0 = Swift_MailTransport::newInstance();        /* Beispiel geht über PHP-Mail, geht i.a. 
+                                                                                 aber keine Information von logger     */
+
+                     $Transport = Swift_SmtpTransport::newInstance('smtp.gmail.com',587,'tls' )     /* 'tls', Ports je nach Server */
+                      ->setUsername("fhnw.weben@gmail.com")
+                      ->setPassword("!Je8Na8Sa9!");
+
+                     $Transport2 = Swift_SmtpTransport::newInstance('mail.gmail.com',995,'tls' )  /* 'tls' */
+                      ->setUsername("...")
+                      ->setPassword("...");
+
+                     $mailer = Swift_Mailer::newInstance($Transport); 
+
+                     // Echo Logger aktivieren (es gibt noch einen logger der auf File schreibt)
+                     $logger = new Swift_Plugins_Loggers_EchoLogger();
+                     $mailer -> registerPlugin ( new Swift_Plugins_LoggerPlugin ($logger));
+
+                     $result = $mailer->send($message);
+
+                    }
+                     catch(Exception $e){
+                       $error_log = $logger->dump();
+                    }
+
+                    if ($result == 0){
+                        die("Mail konnte nicht versandt werden.");
+                    }
+                    
+                }
+                //echo $message->toString();
+
+                header("Content-type: text/html; charset=utf-8");
+                }
+        
+        
         // Datenbankupdate
         $pass = md5($newpwd);
         $neupasswort = "UPDATE user SET `Passwort`='$pass' WHERE `Email`='$email'";
