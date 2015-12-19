3ec8d37f140ace7db2373e55567d3e2c9fd42e3d
Nadine <nadine.toepfer@students.fhnw.ch>
2015-12-19 11:18:36 +0100

    Dropdown Nav-bar funktioniert wieder

--- fh_enrolement.php
+++ fh_enrolement.php
@@ -86,7 +86,7 @@
 <!--Codeteile von Rainer Telesko aus dem Web-Engineering Modul.-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>  
         <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  
+       <!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>  -->
         <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css"/>
         <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js"></script>
         <script id="source" language="javascript" type="text/javascript">
