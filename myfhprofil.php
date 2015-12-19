5666d47525250c0d66be2a8e797fc85ed5c75ce8
Sora22 <Sahra@Study.edu.ds.fhnw.ch>
2015-12-19 14:05:27 +0100

    updated

--- myfhprofil.php
+++ myfhprofil.php
@@ -26,7 +26,7 @@
     <div class = "col-md-7" id="mainBody">
         <h1>Mein FH Profil</h1>
         <p>
-            <a href="fhInfo_update.php?id=$email" class="btn btn-success" role="button">Infos bearbeiten</a>
+            <a href="fhInfo_update.php?email=$email" class="btn btn-success" role="button">Infos bearbeiten</a>
         </p>
         <div class="table-responsive">
             <table>
