9583b4b868bd2ee1ac0b459b8547d05d58f864e3
Sora22 <Sahra@192.168.2.3>
2015-12-20 14:06:13 +0100

    AJAX table for FHprofil

--- ajax_db_fh.php
+++ ajax_db_fh.php
@@ -30,20 +30,3 @@ while ($row = mysqli_fetch_array($result)) {
 echo "</table>";
 mysqli_close($con);
 ?>
-<!--?php
-    header("Content-Type: text/html; charset=utf-8");
-   
-    include 'db.inc.php';
-    $link = mysqli_connect("localhost", $benutzer, $passwort) or die("Keine Verbindung zur Datenbank!");
-            mysqli_select_db($link, $dbname) or die("Datenbank nicht gefunden!". mysql_error());
-    $query = "SELECT * from fh where email = " . $_POST["inst"];
-    $res = mysqli_query($link, $query);
-    $dsatz = mysqli_fetch_assoc($res);
-    
-    echo "<?xml version='1.0' encoding='utf-8'?>\n";
-    echo "<daten>\n";
-    echo "<pa>" . $dsatz["partner"] . "</pa>\n";
-    echo "<em>" . $dsatz["email"] . "</em>\n";
-    echo "</daten>\n";
-    
-?>-->
