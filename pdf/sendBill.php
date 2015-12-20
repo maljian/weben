55fc61273e369aa9bce6080175a05eb76ad3096d
Nadine <nadine.toepfer@students.fhnw.ch>
2015-12-20 21:36:11 +0100

    Mail mit PDF kann verschickt werden

--- pdf/sendBill.php
+++ pdf/sendBill.php
@@ -1,111 +1,110 @@
 <?php
 session_start();
-if(!empty($_SESSION['refn'])){
+if (!empty($_SESSION['refn'])) {
+   
+    include('../credentials.php');
     
 //Quelle: http://wiki.selfhtml.org/wiki/PHP/Anwendung_und_Praxis/Formmailer-Advanced
 // eigene Mailadresse
-$zieladresse = $_SESSION['email'];
+    $zieladresse = $_SESSION['email'];
 
 //Absenderadresse
-$absenderadresse = 'fhnw.weben@gmail.com';
+    $absenderadresse = 'dine.bronxx@gmail.com';
 
 //Absendername
-$absendername = 'FH Portal';
+    $absendername = 'FH Portal';
 
 //Betreff Empfänger und Absender
-$betreff = 'Rechnung FH Portal';
+    $betreff = 'Rechnung FH Portal';
 
 //Weiterleitung nach Absenden
-$urlDankeSeite = 'http://www.dine.bronxx.org';
+    $urlDankeSeite = '../addAd.php';
 
 // Welches Zeichen soll zwischen dem Feldnamen und dem angegebenen Wert stehen
-$trenner = ":\t"; // Doppelpunkt und Tabulator
+    $trenner = ":\t"; // Doppelpunkt und Tabulator
 
-/**
- * Ende Konfigurator
- */
-require_once "../swiftmailer/lib/swift_required.php"; // Swift initialisieren
-
-include('../credentials.php');
-// Create the attachment with your data
-$attachment = Swift_Attachment::fromPath("Rechnung_".$_SESSION['refn'].".pdf", "application/pdf");
-if ($_SERVER['REQUEST_METHOD'] === "GET") {
-
-$message = Swift_Message::newInstance(); // Ein Objekt für die Mailnachricht
-
-$message
-->setFrom(array($absenderadresse => $absendername))
-->setTo(array($zieladresse))
-->setSubject($betreff)
-->attach ($attachment)
-->setBody(
-"Sehr geehrte/r ".$_SESSION['gender']." ".$_SESSION['lastname'].
+    /**
+     * Ende Konfigurator
+     */
+    require_once "../swiftmailer/lib/swift_required.php"; // Swift initialisieren
     
-"Vielen Dank dass Sie Ihre Werbung bei uns schalten möchten.
+    $attachment = Swift_Attachment::fromPath("Rechnung_".$_SESSION['refn'].".pdf", "application/pdf");
+
+    if ($_SERVER['REQUEST_METHOD'] === "GET") {
+
+        $message = Swift_Message::newInstance(); // Ein Objekt für die Mailnachricht
+
+        $message
+                ->setFrom(array($absenderadresse => $absendername))
+                ->setTo(array($zieladresse))
+                ->setSubject($betreff)
+                ->attach ($attachment)
+                ->setBody(
+"Sehr geehrte/r ".$_SESSION['gender']." ".$_SESSION['lastname']."
+
+Vielen Dank dass Sie Ihre Werbung bei uns schalten möchten.
     
 Nachdem Sie den Rechnungsbetrag von CHF ".$_SESSION['amount']." bei uns beglichen haben, werden wir Ihre Werbung ab Ihrem Wunschtermin aufschalten.
 Im Anhang finden Sie Ihre Rechnung mit der Rechnungsnummer ".$_SESSION['refn']." und dem Einzahlungsschein.
 
 Freundliche Grüsse
 
-Ihr FH Portal
-    
-Das Bild wurde in der Datenbank gespeichert.");
+Ihr FH Portal");
 
-$mailtext = "";
+        $mailtext = "";
 
-foreach ($_POST as $name => $wert) {
-if (is_array($wert)) {
-foreach ($wert as $einzelwert) {
-$mailtext .= $name . $trenner . $einzelwert . "\n";
-}
-} else {
-$mailtext .= $name . $trenner . $wert . "\n";
-}
-}
+        foreach ($_POST as $name => $wert) {
+            if (is_array($wert)) {
+                foreach ($wert as $einzelwert) {
+                    $mailtext .= $name . $trenner . $einzelwert . "\n";
+                }
+            } else {
+                $mailtext .= $name . $trenner . $wert . "\n";
+            }
+        }
 
-// Code Ergänzungen: try und catch, logger
-// gemäss: [http://swiftmailer.org/pdf/Swiftmailer.pdf Swiftmailer.pdf]
+        // Code Ergänzungen: try und catch, logger
+        // gemäss: [http://swiftmailer.org/pdf/Swiftmailer.pdf Swiftmailer.pdf]
 
-try {
-/* Mit try und catch können die Fehlerevents differenzierter getestet werden
- *
- * diverse Möglichkeiten für $Transport 
- * smtp mit den gewohnten Optionen, siehe Dokumentation Swiftmailer  
- *  für TLS und SSL  allenfalls phpinfo ob extentiens installiert
- */
+        try {
+            /* Mit try und catch können die Fehlerevents differenzierter getestet werden
+             *
+             * diverse Möglichkeiten für $Transport 
+             * smtp mit den gewohnten Optionen, siehe Dokumentation Swiftmailer  
+             *  für TLS und SSL  allenfalls phpinfo ob extentiens installiert
+             */
 
-$Transport0 = Swift_MailTransport::newInstance();        /* Beispiel geht über PHP-Mail, geht i.a. 
-  aber keine Information von logger */
+            $Transport0 = Swift_MailTransport::newInstance();        /* Beispiel geht über PHP-Mail, geht i.a. 
+              aber keine Information von logger */
 
-$Transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, 'tls') /* 'tls', Ports je nach Server */
-->setUsername($USER)
-->setPassword($PWD);
+            $Transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, 'tls') /* 'tls', Ports je nach Server */
+                    ->setUsername($USER)
+                    ->setPassword($PWD);
 
-$Transport2 = Swift_SmtpTransport::newInstance('mail.gmail.com', 995, 'tls') /* 'tls' */
-->setUsername("...")
-->setPassword("...");
+            $Transport2 = Swift_SmtpTransport::newInstance('mail.gmail.com', 995, 'tls') /* 'tls' */
+                    ->setUsername("...")
+                    ->setPassword("...");
 
-$mailer = Swift_Mailer::newInstance($Transport2);
+            $mailer = Swift_Mailer::newInstance($Transport);
 
-// Echo Logger aktivieren (es gibt noch einen logger der auf File schreibt)
-$logger = new Swift_Plugins_Loggers_EchoLogger();
-$mailer->registerPlugin(new Swift_Plugins_LoggerPlugin($logger));
+            // Echo Logger aktivieren (es gibt noch einen logger der auf File schreibt)
+            $logger = new Swift_Plugins_Loggers_EchoLogger();
+            $mailer->registerPlugin(new Swift_Plugins_LoggerPlugin($logger));
 
-echo $message;
+            $result = $mailer->send($message);
+        } catch (Exception $e) {
+            $error_log = $logger->dump();
+        }
 
-$result = $mailer->send($message);
-} catch (Exception $e) {
-$error_log = $logger->dump();
-}
+        if ($result == 0) {
+            die($_SESSION['contactMessage']='failed');
+            echo $e;
+        }
+  
+        header("Location: $urlDankeSeite");
+        exit;
+    }
+//echo $message->toString();
 
-if ($result == 0) {
-die("Mail konnte nicht versandt werden.");
-}
-header("Location: $urlDankeSeite");
-exit;
-}
-echo $message->toString();
-
-header("Content-type: text/html; charset=utf-8");
+    header("Content-type: text/html; charset=utf-8");
 }
