9900db7f42c89062a940717d77a49275e5df85dd
Sora22 <Sahra@192.168.2.3>
2015-12-20 14:50:52 +0100

    AJAX table for FHprofil

--- ajax_db_fh.php
+++ ajax_db_fh.php
@@ -21,9 +21,8 @@ echo "<table class='table table-striped table-bordered'>
                 </tr>
                 </thread><tbody>";
 while ($row = mysqli_fetch_array($result)) {
-    echo "success!!";
     echo "<tr>";
-    echo "<td>" . $row['instituion'] . "</td>";
+    echo "<td>" . $row['institution'] . "</td>";
     echo "<td>" . $row['city'] . "</td>";
     echo "<td>" . $row['website'] . "</td>";
     echo "<td>" . $row['partner'] . "</td>";
