56310366cf3c6ec7a9fa1c537c4e1267a8bf9a36
Nadine <nadine.toepfer@students.fhnw.ch>
2015-12-21 21:14:20 +0100

    PDF mit Text gefüllt

--- pdf/Kursrechnung/einzahlungsschein.php
+++ pdf/Kursrechnung/einzahlungsschein.php
@@ -2,9 +2,12 @@
 session_start();
 require_once('class.einzahlungsschein.php');
 require_once('../../fpdf/fpdf.php');
-
+date_default_timezone_set("Europe/Zurich");
+$timestamp=time();
+$date= date("d.m.Y",$timestamp);
 $number=$_POST['number'];
 $email = $_SESSION['email'];
+
 require '../../database.php';
 
 $pdo = Database::connect();
@@ -32,9 +35,40 @@ $ref = "3000000000"+$data["id"];
 $pdf = new FPDF('P', 'mm', 'A4');
 $pdf->AddPage();
 $pdf->SetAutoPageBreak(0, 0);
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(190, 4, "","T", 1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(50, 4, "FH Portal",0,1);
+$pdf->Cell(50, 4, "Von-Roll-Str. 10",0, 1);
+$pdf->Cell(50, 4, "4600 Olten", 0,1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(50, 4, $data['institution'],0, 1);
+$pdf->Cell(50, 4, $data['partner'],0, 1);
+$pdf->Cell(50, 4, $data['street'],0, 1);
+$pdf->Cell(50, 4, $data['postalcode']." ".$data['city'],0, 1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(190, 4, "","T", 1);
+$pdf->SetFont('Arial','B',11);
+$pdf->Cell(100, 4, "FH Portal Rechnungsnummer: ".$ref,0);
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(91, 4, $date,0,1,'R');
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->SetFont('Arial', '', 11);
+$pdf->Cell(100, 4, "Sehr geehrte/r Frau/Herr ".$data['partner'],0,1);
+$pdf->Cell(50, 4, "",0, 1);
+$pdf->MultiCell(190, 4, "Sie haben auf dem FH Portal ".$number." Kurse gebucht. Bitte begleichen Sie den Betrag von CHF ".$amount." innerhalb der n".chr(228)."chsten vier Wochen.",0, 1);
+$pdf->Cell(50, 4, "",0, 1);
+$pdf->Cell(190, 4, "Bitte verwenden Sie zur ".chr(220)."berweisung den beigef".chr(252)."gten Einzahlungsschein.",0, 1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(50, 4, "Freundliche Gr".chr(252)."sse",0, 1);
+$pdf->Cell(50, 10, "",0, 1);
+$pdf->Cell(50, 4, "Ihr FH Portal",0, 1);
+$pdf->Cell(50, 10, "",0, 1);
 $pdf->SetFont('Arial', '', 9);
-$pdf->Cell(50, 4,
- "Just some dummy text.");
+$pdf->Cell(50, 4, "Diese Rechnung wurde maschinell erstellt und ist daher ohne Unterschrift g".chr(252)."ltig.",0, 1);
 
 //now simply include your Einzahlungsschein, sending your pdf instance to the Einzahlungsschein class
 $ezs = new Einzahlungsschein(196, 0, $pdf);
