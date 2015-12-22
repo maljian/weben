b0585bbe7510e899156a218df2ab6defa6f4a52d
Jenny <Jenny@10.0.1.25>
2015-12-22 17:49:02 +0100

    reset password fixed

--- alerts/reset_alert.php
+++ alerts/reset_alert.php
@@ -31,6 +31,16 @@ if (isset($_SESSION['resetMessage']) AND $_SESSION['resetMessage']=="failed")
                 </div>
             </div>";
         unset($_SESSION['resetMessage']);
-        }        
+        }  
+if (isset($_SESSION['resetMessage']) AND $_SESSION['resetMessage']=="too short")
+        {
+        echo "<div class=\"panel-group\">
+                <div id=\"resetTooShort\" class=\"alert alert-danger\">
+                    <a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>
+                    <strong>Das neue Passwort ist zu kurz! Bitte wählen Sie ein Passwort mit mindestens 8 Zeichen.</strong>
+                </div>
+            </div>";
+        unset($_SESSION['resetMessage']);
+        }
 ?>
 
--- login/reset_password.php
+++ login/reset_password.php
@@ -7,37 +7,44 @@
        $confirmPwd = $_POST['confirmPwd'];
        $email = $_SESSION['email'];
        
-       $pdo = Database::connect();
-       $pdo->exec('set names utf8');
-       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       //check if old password is correct
-       $sql= "SELECT Passwort FROM user WHERE Email = '$email'";
-       $resultUser = $pdo->prepare($sql);
-       $resultPW = $resultUser->execute();
-       if($resultPW == $oldPwd){
+       if(strlen($newPwd)>=8){
+            $pdo = Database::connect();
+            $pdo->exec('set names utf8');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            //check if old password is correct
             $sql= "SELECT * FROM user WHERE Email = '$email'";
-            $resultUser = $pdo->prepare($sql);
-            $resultUser->execute();
-            if($resultUser->rowCount()==1) {   
-                //check if new password matches confirm password and update DB 
-                if($newPwd==$confirmPwd){
-                    $newPwd = md5($newPwd);
-                    $sql = "UPDATE user SET Passwort='$newPwd' WHERE Email='$email'";
-                    $q = $pdo->prepare($sql);
-                    $q->execute();
-                    Database::disconnect();
-                    header("Location: ../myfhprofil.php");
-                }
-            }
-        
-            $_SESSION['resetMessage']= 'successful';
-            header("Location: ../reset.php");
-        }
+            $q = $pdo->prepare($sql);
+            $q->execute(array($email));
+            $data = $q->fetch(PDO::FETCH_ASSOC);
+            $resultPW = $data['Passwort'];
+            $oldPwd = md5($oldPwd);
+            
+            if($resultPW == $oldPwd){
+                $sql= "SELECT * FROM user WHERE Email = '$email'";
+                $resultUser = $pdo->prepare($sql);
+                $resultUser->execute();  
+                    //check if new password matches confirm password and update DB 
+                    if($newPwd==$confirmPwd){
+                        $newPwd = md5($newPwd);
+                        $sql = "UPDATE user SET Passwort='$newPwd' WHERE Email='$email'";
+                        $q = $pdo->prepare($sql);
+                        $q->execute();
+                        Database::disconnect();
+                        
+                        $_SESSION['resetMessage']= 'successful';
+                        header("Location: ../myfhprofil.php");
+                    }
+             }
+             else {
+                 $_SESSION['resetMessage']= 'wrong password';
+                 header("Location: ../reset.php");
+             }
+        } 
         else {
-            $_SESSION['resetMessage']= 'wrong password';
+            $_SESSION['resetMessage'] = 'too short';
             header("Location: ../reset.php");
         }
-       
     }
     else {
     $_SESSION['resetMessage']= 'failed';
