b0d3c9353e4dc242896ff7bc52776334625b512c
Jenny <Jenny@10.0.1.25>
2015-12-22 12:07:48 +0100

    reset password admin added

---
+++ login/reset_password_admin.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    require '../database.php';
+    if(isset($_POST)){
+       $oldPwd = $_POST['oldPwd'];
+       $newPwd = $_POST['newPwd'];
+       $confirmPwd = $_POST['confirmPwd'];
+       $email = $_SESSION['email'];
+       
+       $pdo = Database::connect();
+       $pdo->exec('set names utf8');
+       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       //check if old password is correct
+       $sql= "SELECT Passwort FROM user WHERE Email = '$email'";
+       $resultUser = $pdo->prepare($sql);
+       $resultPW = $resultUser->execute();
+       if($resultPW == $oldPwd){
+            $sql= "SELECT * FROM user WHERE Email = '$email'";
+            $resultUser = $pdo->prepare($sql);
+            $resultUser->execute();
+            if($resultUser->rowCount()==1) {   
+                //check if new password matches confirm password and update DB 
+                if($newPwd==$confirmPwd){
+                    $newPwd = md5($newPwd);
+                    $sql = "UPDATE user SET Passwort='$newPwd' WHERE Email='$email'";
+                    $q = $pdo->prepare($sql);
+                    $q->execute();
+                    Database::disconnect();
+                    $_SESSION['resetMessage']= 'successful';
+                    header("Location: ../reset_admin.php");
+                }
+            }
+        }
+        else {
+            $_SESSION['resetMessage']= 'wrong password';
+            header("Location: ../reset_admin.php");
+        }
+       
+    }
+    else {
+    $_SESSION['resetMessage']= 'failed';
+    header("Location: ../reset_admin.php");   
+    }    
+?>
+
